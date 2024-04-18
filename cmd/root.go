2b39b139244ada558695f3f1e987704d3e7e774d
Anthony Camarillo <anthony.camarillo.96@gmail.com>
2024-04-18 15:51:31 -0700

    add version flag to broom

--- cmd/root.go
+++ cmd/root.go
@@ -2,19 +2,18 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
   Use: "broom",
+  Version: "0.3.0",
   Short: "Broom sweeps up your local repository branches",
   Long:"A terminal user interface tool for cleaning up your repository's local branches.\n" +
        "Includes options for viewing remote references.\n" +
        "Documentation can be found at https://pkg.go.dev/github.com/a-camarillo/broom.",
   Run: func(cmd *cobra.Command, args[]string) {
-    //TODO add commands lmao
     if len(args) == 0 {
       cmd.Help()
     }
@@ -24,6 +23,5 @@ var rootCmd = &cobra.Command{
 func Execute() {
   if err := rootCmd.Execute(); err != nil {
     fmt.Println(err)
-    os.Exit(1)
   }
 }
